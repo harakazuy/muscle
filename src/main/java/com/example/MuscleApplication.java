1b076fbe60483f52f22252993e087b4e606a2228
kazuya.hara <kzyn400r@gmail.com>
2017-07-31 14:50:50 +0900

    デプロイ用コミット

--- src/main/java/com/example/MuscleApplication.java
+++ src/main/java/com/example/MuscleApplication.java
@@ -2,13 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class MuscleApplication {
+public class MuscleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MuscleApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MuscleApplication.class);
 	}
 }
