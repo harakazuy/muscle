3dcb02211859f0fc221853f6f6c926cccbfa7d4a
kazuya.hara <kzyn400r@gmail.com>
2017-08-03 14:30:32 +0900

    筋トレ記録フォーム用の関数名を分かりやすくした。更新画面のキャンセルのリンクをコンテキストパスを使用するようにした。

--- src/main/resources/static/js/record-form.js
+++ src/main/resources/static/js/record-form.js
@@ -1,62 +1,73 @@
 /**
  * 
  */
-		$("#header-outer").load("header #header");
-		$("#navbar-outer").load("navbar #navbar");
-		$("#footer-outer").load("footer #footer");
-		var form;
-		function setTraininForm(page){
-			$("#form-outer").load("form #trainingForm", function(){
-				appendOption();
-				form = $('#trainingForm').html();
-				if(page == "index") appendForm(form);
-			});
-		};
-		function appendOption(){
-			for(var i = 1; i <= 50; i++){
-				$("[name='repetition']").append('<option value=\"' + i + '\">' + i + '</option>')
-			}
-			for(var i = 1; i <= 20; i++){
-				$("[name='setCount']").append('<option value=\"' + i + '\">' + i + '</option>')
-			}
-		};
-		function formValidation(){
-			var msg_required="必須入力です。";
-			
-			$("[name='date']").attr("data-validation", "required");
-			$("[name='date']").attr("data-validation-error-msg", '日時は' + msg_required);
-			
-			$("[name='trainingId']").attr("data-validation", "required");
-			$("[name='trainingId']").attr("data-validation-error-msg", 'メニューは' + msg_required);
-			
-			$("[name='repetition']").attr("data-validation", "required");
-			$("[name='repetition']").attr("data-validation-error-msg", '回数は' + msg_required);
-			
-			$("[name='setCount']").attr("data-validation", "required");
-			$("[name='setCount']").attr("data-validation-error-msg", 'セット数は' + msg_required);
 
-			$.validate({
-			    errorMessagePosition : 'top',
-			    language : {
-			    	errorTitle: 'フォームの送信に失敗しました'
-			    }
-			});
-		};
-		formValidation();
-		function removeCurrent(){
-			$('.removeCurrent').click(function(){
-				$(this).parent().parent().remove();
-			})
-		};
-		removeCurrent();
-		function appendForm(fm){
-			$('#form').append(fm);
-			formValidation();
-			removeCurrent();
-		};
-		$('#appendForm').click(function(){
-			appendForm(form);
-		});
-		$('#removeForm').click(function(){
-			$('#form div:last-child').remove();
-		});
+$("#header-outer").load("header #header");
+$("#navbar-outer").load("navbar #navbar");
+$("#footer-outer").load("footer #footer");
+
+var form;
+
+function setTrainingForm(page){
+	$("#form-outer").load("form #trainingForm", function(){
+		appendRepAndCount();
+		form = $('#trainingForm').html();
+		if(page == "index") appendForm(form);
+	});
+};
+
+function appendRepAndCount(){
+	for(var i = 1; i <= 50; i++){
+		$("[name='repetition']").append('<option value=\"' + i + '\">' + i + '</option>')
+	}
+	for(var i = 1; i <= 20; i++){
+		$("[name='setCount']").append('<option value=\"' + i + '\">' + i + '</option>')
+	}
+};
+
+function formValidation(){
+	var msg_required="必須入力です。";
+	
+	$("[name='date']").attr("data-validation", "required");
+	$("[name='date']").attr("data-validation-error-msg", '日時は' + msg_required);
+	
+	$("[name='trainingId']").attr("data-validation", "required");
+	$("[name='trainingId']").attr("data-validation-error-msg", 'メニューは' + msg_required);
+	
+	$("[name='repetition']").attr("data-validation", "required");
+	$("[name='repetition']").attr("data-validation-error-msg", '回数は' + msg_required);
+	
+	$("[name='setCount']").attr("data-validation", "required");
+	$("[name='setCount']").attr("data-validation-error-msg", 'セット数は' + msg_required);
+
+	$.validate({
+	    errorMessagePosition : 'top',
+	    language : {
+	    	errorTitle: 'フォームの送信に失敗しました'
+	    }
+	});
+};
+
+formValidation();
+
+function removable(){
+	$('.removeRow').click(function(){
+		$(this).parent().parent().remove();
+	})
+};
+
+removable();
+
+function appendForm(formElement){
+	$('#form').append(formElement);
+	formValidation();
+	removable();
+};
+
+$('#appendForm').click(function(){
+	appendForm(form);
+});
+
+$('#removeForm').click(function(){
+	$('#form div:last-child').remove();
+});
